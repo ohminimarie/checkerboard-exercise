9e8e318439464b3ff42ac8b0d67d7e5b754f7213
ohminimarie <mcjalcantara@gmail.com>
2015-11-08 16:33:18 -0800

    Checkerboard solution

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Your JS goes here
+function board(color) {
+
+var newDiv = document.createElement("div");
+document.body.appendChild(newDiv);
+
+newDiv.style.width = "11.1%";
+newDiv.style.float = "left";
+newDiv.style.paddingBottom = "11.1%";
+newDiv.style.backgroundColor = color;
+
+};
+
+
+for (var i = 1; i < 64; i++) {
+  if (i%2===0) {
+    board("red");
+  } else {
+    board("black");
+  }
+};
+
+
