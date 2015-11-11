156abfb61bcc693b4b084b1a10bf5239f6e6c469
ohminimarie <mcjalcantara@gmail.com>
2015-11-11 11:33:02 -0800

    I'm bad at version control.

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-function board(color) {
+var divs = document.getElementsByTagName('div');
+var flash = true;
 
-var newDiv = document.createElement("div");
-document.body.appendChild(newDiv);
-
-newDiv.style.width = "11.1%";
-newDiv.style.float = "left";
-newDiv.style.paddingBottom = "11.1%";
-newDiv.style.backgroundColor = color;
-
-};
-
-
-for (var i = 1; i < 64; i++) {
-  if (i%2===0) {
-    board("red");
-  } else {
-    board("black");
+function checkerBoard() {  
+  for (var i = 0; i < 72; i++) {
+    document.body.appendChild(document.createElement('div'));
+    divs[i].style.width = "11.1%";
+    divs[i].style.paddingBottom = "11.1%";
+    divs[i].style.float = "left";
   }
 };
+
+function colors() {
+  var dry = function(gradient) {
+    for (var i = 0; i < divs.length; i++) {
+
+      if (gradient) {
+        color1 = 'rgb(100, 0,' + 6*i + ')';
+        color2 = 'rgb(0, 200,' + 6*i + ')';
+      } else {
+        color1 = 'rgb(0, 0, 0)';
+        color2 = 'rgb(255, 255, 255)';
+      };
+
+      if (i % 2 === 0) {
+        divs[i].style.backgroundColor = color1; 
+      } else {
+        divs[i].style.backgroundColor = color2;
+      }
+    }
+  }
+
+  dry(flash);
+  flash = !flash;
+};
+
+
+checkerBoard();
+window.setInterval(colors, 500);
